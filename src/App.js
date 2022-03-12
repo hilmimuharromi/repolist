dc5856963dd59a2ce58e9519fc7a484ea501aa78
hilmimuharromi <admin@admins-MacBook-Pro.local>
2022-03-12 13:23:14 +0700

    styling search input & base css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import "./App.css"
 import { SearchInput, ListRepo } from './components'
 export const App = () => {
   return (
-    <div>
-      <SearchInput />
-      <ListRepo />
+    <div className='layout'>
+      <div className='container'>
+        <SearchInput />
+        <ListRepo />
+      </div>
     </div>
   )
 }
--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const SearchInput = () => {
-  return (
-    <div>
-        <input 
-        placeholder='Search by Username'/>
-        <buttton>Search</buttton>
-    </div>
-  )
+    const [input, setInput] = useState('')
+    return (
+        <form
+            className='flex justify-center'
+            onSubmit={(e) => {
+                e.preventDefault()
+                console.log('masuk', input)
+            }
+            }>
+            <input
+                className='mr-10 rounded-sm w-20 shadow'
+                placeholder='Search by Username' 
+                onChange={(e) => setInput(e.target.value)}
+                />
+            <button
+                className='flex justify-center bg-primary shadow p-10 rounded-sm w-10'
+                type='submit'
+            >Search</button>
+        </form>
+    )
 }
 
 export default SearchInput
